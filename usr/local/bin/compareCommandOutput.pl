ab55009b9a4367eaebf3ea2106141dddb824c738
kai <kai@hari.fritz.box>
2025-10-17 01:42:53 +0200

    initial

---
+++ usr/local/bin/compareCommandOutput.pl
@@ -0,0 +1,160 @@
+#!/usr/bin/perl
+
+# Requires: apt install libnotify-bin
+
+use strict;
+use warnings;
+use IPC::Run qw(run);
+use Data::Dumper;
+
+my $command = shift or die "ERROR: No command specified";
+print "INFO: command: $command\n";
+my $mode = shift || 'compare';
+my $urgency = shift || 'normal';
+my $time = shift || 3000;
+
+
+my $refOutputDir = "/var/local/localmon";
+print "INFO: refOutputDir: $refOutputDir\n";
+
+main(
+     $command,
+     $mode,
+     $refOutputDir,
+     $urgency,
+     $time
+    );
+
+sub main
+{
+    my $command = shift;
+    my $mode = shift;
+    my $refOutputDir = shift;
+    my $urgency = shift;
+    my $time = shift;
+
+    my $refFileName = $command;
+    $refFileName =~ s/\W/_/g;
+    my $refFilePath = "$refOutputDir/$refFileName";
+    print "INFO: refFilePath: $refFilePath\n";
+
+    if (-e $refFilePath)
+    {
+        my ($result, $ret) = compareCommandOutput(
+                                                  $command,
+                                                  $refFilePath
+                                                 );
+
+        print "INFO: result:\n$result\n";
+
+        if ($ret)
+        {
+            notify(
+                   "localmon",
+                   "Change in <$command>",
+                   $result,
+                   $urgency,
+                   $time
+                  );
+
+        }
+    }
+
+    if ($mode eq 'save')
+    {
+        saveCommandOutput(
+                          $command,
+                          $refFilePath
+                         );
+    }
+    else
+    {
+        die "ERROR: Reference file '$refFilePath' does not exist" unless -e $refFilePath;
+    }
+}
+
+sub saveCommandOutput
+{
+    my $command = shift;
+    my $refFilePath = shift;
+
+    my @cmd = split /\s+/, $command;
+    print "EXEC: @cmd";
+
+    my $stdout;
+    run \@cmd, \undef, \$stdout or die $?;
+    print "INFO: stdout:\n$stdout\n";
+
+    open OUT, ">$refFilePath";
+    print OUT $stdout;
+    close OUT;
+
+    print "INFO: Command output of '$command' successfully saved to '$refFilePath'."
+}
+
+sub notify
+{
+    my $appName = shift;
+    my $summary = shift;
+    my $body = shift;
+    my $urgency = shift;
+    my $time = shift;
+
+    my @cmd = (
+               "sudo",
+               "-u kai",
+               "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus",
+               "notify-send",
+               "-a '$appName'",
+               "-u $urgency",
+               "-t $time",
+               "'$summary'",
+               "\'$body\'"
+              );
+
+    my $cmd = join ' ', @cmd;
+
+    print "EXEC: $cmd\n";
+    system $cmd;
+}
+
+sub compareCommandOutput
+{
+    my $command = shift;
+    my $refFilePath = shift;
+
+
+    my @cmd1 = split /\s+/, $command;
+    my @cmd2 = split /\s+/, "diff -u0 $refFilePath -";
+    print "EXEC: @cmd1 | @cmd2\n";
+
+    print Dumper \@cmd1, \@cmd2;
+
+    my $stdout;
+    run \@cmd1, '|', \@cmd2, \$stdout;
+    my $ret = $?;
+    print "INFO: ret: $ret\n";
+
+    my $result = removeUninterestingLines($stdout);
+
+    return $result, $ret;
+}
+
+sub removeUninterestingLines
+{
+    my $input = shift;
+
+    my @result;
+    foreach my $line (split /\n/, $input)
+    {
+        next unless $line =~ /^(\+|\-)/;
+        next if $line =~ /^(\+\+\+|\-\-\-)/;
+        $line =~ s/-/\\-/g;
+        #print "LINE: $line\n";
+        push @result, $line;
+    }
+
+    my $result = join "\n", @result;
+
+    return $result;
+}
